c0a2ed97f74fca8d241893c318965862dfb9a1e8
양시연 <ssionii353@gmail.com>
2021-03-25 14:03:21 +0900

    Subjects 실습 및 Rxcocoa import

--- RxSwiftPractice/RxSwiftPractice.playground/Contents.swift
+++ RxSwiftPractice/RxSwiftPractice.playground/Contents.swift
@@ -1,5 +1,13 @@
 import UIKit
 import RxSwift
+import RxCocoa
+
+enum MyError: Error {
+    case anError
+}
+
+
+//MARK: - 02_Observables
 
 example(of: "just, of, from") {
     let one = 1
@@ -89,10 +97,6 @@ example(of: "DisposeBag") {
         .disposed(by: disposeBag) // 이렇게 dispose를 해야 메모리 누수가 안일어남
 }
 
-enum MyError: Error {
-    case anError
-}
-
 example(of: "create") {
     let disposeBag = DisposeBag()
     
@@ -180,6 +184,129 @@ example(of: "Single") {
             case .failure(let error):
                 print(error)
             }
+        }
+        .disposed(by: disposeBag)
+}
+
+//MARK: - 03_Subjects
+
+example(of: "PublishSubject") {
+    let subject = PublishSubject<String>()
+    
+    subject.onNext("Is anyone listening?")
+    
+    // next 이벤트가 발생했을 때 어떻게 할 건지 정의 해둠
+    let subscriptionOne = subject
+        .subscribe(onNext: { string in // subscribe로 구독 !
+            print(string)
+        })
+    
+    // next 이벤트 발생시키기
+    subject.onNext("1")
+}
+
+example(of: "PublishSubject") {
+    // subject 생성
+    let subject = PublishSubject<String>()
+    subject.onNext("Is anyone listening?")
+    
+    // subject를 구독하고 next 이벤트가 왔을 때 어떻게 처리할지 정의
+    let subscriptionOne = subject
+        .subscribe(onNext: { string in
+            print(string)
+        })
+    subject.onNext("1")
+    subject.onNext("2")
+    
+    // subject를 구독하고 이벤트가 왔을 때 어떻게 처리할지 정의
+    let subscriptionTwo = subject
+        .subscribe({ event in
+            print("2)", event.element ?? event)
+        })
+    
+    subject.onNext("3")
+
+    // subscriptionOne dispose
+    subscriptionOne.dispose()
+    subject.onNext("4")
+    
+    // subject 끝 ! 이제 방출 안할거다 !
+    // subject는 이러한 종료 이벤트들을 이후 새로운 subscriber들에게 재방출한다 ! (subject가 작동하는 것은 x)
+    subject.onCompleted()
+    
+    subject.onNext("5")
+    
+    // subscriptionTwo dispose
+    subscriptionTwo.dispose()
+    
+    // 쓰레기통 만들기
+    let disposeBag = DisposeBag()
+    
+    // subject를 구독하고 이벤트가 왔을 때 어떻게 처리할지 정의
+    subject
+        .subscribe { event in
+            print("3)", event.element ?? event)
+        }
+        .disposed(by: disposeBag) // 일일히 dispose 하지 않고, observable에 대해 subscribing하고 이것을 즉히 쓰레기통에 추가
+    
+    subject.onNext("?")
+}
+
+func print<T: CustomStringConvertible>(label: String, event: Event<T>) {
+    print(label, event.element ?? event.error ?? event)
+}
+
+example(of: "BehaviorSubject") {
+    
+    // BehaviorSubject는 항상 최신의 값을 방출하기 때문에 초기값 없이는 만들 수 없다. 반드시 초기값이 있어야 함
+    let subject = BehaviorSubject(value: "Initial value")
+    let disposeBag = DisposeBag()
+    
+    subject.onNext("X")
+    subject
+        .subscribe { event in
+            print(label: "1)", event: event)
+        }
+        .disposed(by: disposeBag)
+    
+    subject.onError(MyError.anError)
+    
+    subject
+        .subscribe { event in
+            print(label: "2)", event: event)
+        }
+        .disposed(by: disposeBag)
+}
+
+example(of: "ReplaySubject") {
+    let subject = ReplaySubject<String>.create(bufferSize: 2)
+    let disposeBag = DisposeBag()
+    
+    subject.onNext("1")
+    subject.onNext("2")
+    subject.onNext("3")
+    
+    subject
+        .subscribe { event in
+            print(label: "1)", event: event)
+        }
+        .disposed(by: disposeBag)
+    
+    subject
+        .subscribe { event in
+            print(label: "2)", event: event)
+        }
+        .disposed(by: disposeBag)
+    
+    subject.onNext("4")
+    
+    subject.onError(MyError.anError)
+    
+    subject.dispose()
+    
+    subject
+        .subscribe { event in
+            print(label: "3)", event: event)
         }
         .disposed(by: disposeBag)
 }
